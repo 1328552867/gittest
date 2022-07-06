2774ce6908cbdf0900e7dfb4e6fe0d35af340b37
游吟 <54663988+1328552867@users.noreply.github.com>
2022-07-06 21:00:05 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,7 +7,7 @@ public class GitTest {
         System.out.println("123");
         System.out.println("321");
         System.out.println("321");
-        System.out.println("123");
+        System.out.println("哈哈哈");
         System.out.println("哈哈哈");
     }
 }
